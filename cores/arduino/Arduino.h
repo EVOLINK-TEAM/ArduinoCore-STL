0bb026646e30fbcd9054090cdff534a5f92b2bcd
AEva <crystar.z@outlook.com>
2024-07-20 13:28:15 +0800

    ✨ feat: add DLOG

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -10,4 +10,19 @@ int atexit(void (* /*func*/)()) __attribute__((weak));
 #endif // abs
 #define abs(x) ((x) > 0 ? (x) : -(x))
 
+#ifdef ENABLE_DLOG
+#define DLOG(dom, msg)                  \
+    {                                   \
+        Serial.print("\33[31m\33[1m["); \
+        Serial.print(dom);              \
+        Serial.print("]\33[0m");        \
+        Serial.print(msg);              \
+        Serial.print("\33[32m(");       \
+        Serial.print(micros() / 1000);  \
+        Serial.println("ms)\33[0m");    \
+    }
+#else
+#define DLOG(...)
+#endif
+
 using namespace arduino;
